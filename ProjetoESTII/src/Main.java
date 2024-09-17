b2059e77edd4e31bf9e97d90799b5a1689d94349
Vinicius A. Marques <vini.amarques10@gmail.com>
2024-09-17 17:32:42 -0300

    CriacaoBase

---
+++ ProjetoESTII/src/Main.java
@@ -0,0 +1,10 @@
+//Nomes:
+//Vinicius Alves Marques RA: 10417880
+//Gabriel Barros Albertini RA: 10419482
+//Rafael de Menezes Rossi RA: 10417954
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //Menu e chamadas (usando switch)
+    }
+}
